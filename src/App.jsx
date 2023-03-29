0a2f0df9bdb83f563d81aae33b04a9d42bf8037c
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2023-03-29 13:07:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+const options = [
+  {id:0, name: "Piedra", emoji: "🪨", beats: [2,3]},
+  {id:1, name: "Papel", emoji: "📃", beats: [0]},
+  {id:2, name: "Tijera", emoji: "✂️", beats: [1,3]},
+  {id:3, name: "Lagarto", emoji: "🦎", beats: [1]},
+  {id:4, name: "Spock", emoji: "🖖", beats: [3,0]}
+]
+
+export default function App() {
+
+
+  return (
+      <div>
+        Hola mundo
+    </div>
+  )
+}
+
